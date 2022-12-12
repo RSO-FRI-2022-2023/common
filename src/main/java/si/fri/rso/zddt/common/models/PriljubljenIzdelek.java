5ec18ede9b571909b163dbe175d7e2dc660519dc
ziga7631 <zd7631@student.uni-lj.si>
2022-12-12 15:23:12 +0100

    PriljubljenIzdelek

---
+++ src/main/java/si/fri/rso/zddt/common/models/PriljubljenIzdelek.java
@@ -0,0 +1,38 @@
+package si.fri.rso.zddt.common.models;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "priljubljen_izdelek")
+@NamedQueries(value =
+        {
+                //vrni vse izdelke
+                @NamedQuery(name = "PriljubljenIzdelek.getAll",
+                        query = "SELECT i FROM PriljubljenIzdelek i"),
+                //vrni izdelek - id
+                @NamedQuery(name = "PriljubljenIzdelek.getById",
+                        query = "SELECT i FROM PriljubljenIzdelek i WHERE i.id = :idPriljubljenIzdelek"),
+                //vrni izdelek - kategorija
+                @NamedQuery(name = "PriljubljenIzdelek.getIzdelekId",
+                        query = "SELECT i FROM PriljubljenIzdelek i WHERE i.izdelek = :izdelekId"),
+                //vrni izdelek - naziv
+                @NamedQuery(name = "Izdelek.getByUser",
+                        query = "SELECT i FROM PriljubljenIzdelek i WHERE i.uporabnik.id = :uporabnikId")
+        })
+public class PriljubljenIzdelek {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "priljubljen_izdelek_id")
+    private Integer id;
+
+    @OneToOne
+//    @JoinColumn(name = "izdelek_id")
+    private Izdelek izdelek;
+
+    @OneToOne
+//    @JoinColumn(name = "user_id")
+    private Uporabnik uporabnik;
+
+}
